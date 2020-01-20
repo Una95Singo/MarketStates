19bd5881cc511c5b7a75df3cb9246801043fe536
Unarine Singo <una.singo@gmail.com>
2020-01-20 22:35:13 +0200

    Viterbi Algorithm

---
+++ R/viterbi.R
@@ -0,0 +1,65 @@
+# Viterbi algorithm
+# una singo, SNGUNA003
+# 9 January 2020
+# viterbi function finds the optimal path given a matrix of K states over T time points
+
+
+
+#' @param D matrix of 
+#' @param K number of states
+#' @param gamma temporal consistency parameter
+viterbi <- function(D, K, gamma) {
+  Time = nrow(D)
+  
+  # initialize
+  PrevCost = rep(0,K)
+  CurrentCost = rep(0, K)
+  
+  FinalMinVal = NA
+  FinalPath = NA
+  
+  PrevPath = list ()
+  PrevPath[[1]] = NA
+  PrevPath[[2]] = NA
+  PrevPath[[3]] = NA
+  PrevPath[[4]] = NA
+  
+  CurrentPath = list()
+  CurrentPath[[1]] = NA
+  CurrentPath[[2]] = NA
+  CurrentPath[[3]] = NA
+  CurrentPath[[4]] = NA
+  
+  # Viterbi loop
+  for ( t in 1:Time){
+    for (k in 1:K){
+      MinVal = which(PrevCost == min(PrevCost))[1]
+      if((PrevCost[MinVal]+gamma) > PrevCost[k]){
+        CurrentCost[k] = PrevCost[k] + D[t,k]
+        CurrentCost = unlist(CurrentCost)
+        CurrentPath[[k]] = c(PrevPath[[k]], k) 
+      }
+      else{
+        CurrentCost[k] = PrevCost[MinVal] + gamma  + D[t,k]
+        CurrentCost = unlist(CurrentCost)
+        CurrentPath[[k]] = c(PrevPath[[MinVal]], k) 
+      }
+      
+      
+    }
+    #update after iterating throuh the states. the paper updates before the state ends.
+    PrevCost = CurrentCost
+    PrevPath = CurrentPath
+  }
+  
+  
+  CurrentCost
+  FinalMinVal = which(CurrentCost == min(CurrentCost))[1]
+  FinalPath = CurrentPath[[FinalMinVal]]
+  FinalPath = na.omit(FinalPath)
+  
+  # return the final path and cost pair of the viterbi algorithm
+  return(list('Final_Path' = FinalPath, 'Final_Cost'= min(CurrentCost)))
+}
+
+
